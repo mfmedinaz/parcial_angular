22050b91e00c46b0b3255d07099d31e721c3e473
Mario <mf.medinaz@uniandes.edu.co>
2022-11-04 16:49:49 -0500

    finalizado

--- parcial/src/app/plant/plant-list/plant-list.component.spec.ts
+++ parcial/src/app/plant/plant-list/plant-list.component.spec.ts
@@ -2,27 +2,51 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from '@faker-js/faker';
+import { HttpClientModule } from '@angular/common/http';
 
 import { PlantListComponent } from './plant-list.component';
+import { Plant } from '../plant';
 
 describe('PlantListComponent', () => {
   let component: PlantListComponent;
   let fixture: ComponentFixture<PlantListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PlantListComponent ]
-    })
-    .compileComponents();
+      imports: [HttpClientModule],
+      declarations: [PlantListComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PlantListComponent);
     component = fixture.componentInstance;
+
+    for (let i = 0; i < 3; i++) {
+      const plant = new Plant(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence()
+      );
+
+      component.plants.push(plant);
+    }
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have 3 table rows and a header', () => {
+    expect(debug.queryAll(By.css('tr'))).toHaveSize(4);
   });
 });
--- parcial/src/app/plant/plant-list/plant-list.component.ts
+++ parcial/src/app/plant/plant-list/plant-list.component.ts
@@ -9,13 +9,31 @@ import { PlantService } from '../plant.service';
 })
 export class PlantListComponent implements OnInit {
   plants: Array<Plant> = [];
+  numberInt: number = 0;
+  numberExt: number = 0;
 
   constructor(private plantService: PlantService) {}
 
   getPlants(): void {
     this.plantService.getPlants().subscribe((plants) => {
       this.plants = plants;
+      this.countType();
     });
+  }
+
+  countType(): void {
+    let numberIntF = 0;
+    let numberExtF = 0;
+    this.plants.forEach(function (value) {
+      if (value.tipo === 'Interior') {
+        numberIntF++;
+      } else if (value.tipo === 'Exterior') {
+        numberExtF++;
+      }
+    });
+
+    this.numberInt = numberIntF;
+    this.numberExt = numberExtF;
   }
 
   ngOnInit() {
